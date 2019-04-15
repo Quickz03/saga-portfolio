e75d3100c768b22b789e46b1e7ee9535b998a523
Brian Zilka <Bzilks@gmail.com>
2019-04-15 09:34:52 -0500

    added image source to database.sql

--- database.sql
+++ database.sql
@@ -21,12 +21,12 @@ VALUES ('React'), ('jQuery'), ('Node'), ('SQL'), ('Redux'), ('HTML');
 INSERT INTO "projects"
     ("name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
 VALUES
-    ('Redux-Saga-Garden', 'made an epic garden using react-redux sagas', 'picture', 'url', 'https://github.com/Quickz03/redux-saga-garden', '4-11-19', 5 );
+    ('Redux-Saga-Garden', 'made an epic garden using react-redux sagas', 'images/Redux-saga-garden.png', 'url', 'https://github.com/Quickz03/redux-saga-garden', '4-11-19', 5 );
 
 INSERT INTO "projects"
     ("name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
 VALUES
-    ('Redux-Gallery', 'made app using react with the ability to click on a picture and flip to a description of that picture, and like the picture', 'picture', 'url', 'https://github.com/Quickz03/react-gallery', '3-31-19', 4 );
+    ('Redux-Gallery', 'made app using react with the ability to click on a picture and flip to a description of that picture, and like the picture', 'images/React-gallery.png', 'url', 'https://github.com/Quickz03/react-gallery', '3-31-19', 4 );
 
 
 
